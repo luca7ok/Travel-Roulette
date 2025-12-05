907972cfd60346a89f6b58aeb4cc62ce65ca334c
Tomsa Luca <lucasebastian777@gmail.com>
2025-12-05 17:35:36 +0200

    initial commit

---
+++ PoliHack18/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace PoliHack18;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "PoliHack18" };
+    }
+}
