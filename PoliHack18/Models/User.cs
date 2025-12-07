5a7800283a5d0529940721cd2011c8f5fa356699
jucannicolas <jucannicolas@yahoo.com>
2025-12-07 03:18:05 +0200

    sign-up added

--- PoliHack18/Models/User.cs
+++ PoliHack18/Models/User.cs
@@ -1,3 +1,4 @@
+
 namespace PoliHack18.Models
 {
     public class User
@@ -5,5 +6,9 @@ namespace PoliHack18.Models
         public string ID { get; set; } = Guid.NewGuid().ToString();
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string PhoneNumber {  get; set; } = string.Empty;
+
+        public string Name {  get; set; } = string.Empty;
     }
 }
---
+++ PoliHack18/Repository/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using PoliHack18.Models;
+using PoliHack18.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliHack18.Repository
+{
+    public class UserRepository
+    {
+        public async Task AddUser(User user)
+        {
+            string query =
+            "INSERT INTO users ( \"email\", \"password\", \"name\", age, \"phone_number\") " +
+            "VALUES (@email, @password, @name, @age, @telephone_number)";
+            string hashpassword = Password.HashPassword(user.Password);
+            var parameters = new Dictionary<string, object>
+                {
+                    {"@email",user.Email},
+                    {"@password",hashpassword },
+                    {"@name",user.Name},
+                    {"@age",user.Age},
+                    {"@telephone_number",user.PhoneNumber }
+                };
+            await Task.Run(() => Database.ExecutaNonQuery(query, parameters));
+        }
+    }
+}
